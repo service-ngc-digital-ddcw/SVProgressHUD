eec0ca055d6bc20ca9681dee44dc936877e9025e
Paul Booth <206081+paulb00th@users.noreply.github.com>
2022-06-29 15:40:53 +0100

    add Package.swift

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "SVProgressHUD")
+            name: "SVProgressHUD",
+            path: "SVProgressHUD")
     ]
 )
